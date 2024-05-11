59b20788d10d98077f52e1b16a6ff96099a2daad
jeremias mazzetti <jere.mazzetti@gmail.com>
2024-05-11 18:22:49 -0300

    cambio de nombre de archivos para evitar errores.

--- App.tsx
+++ App.tsx
@@ -12,8 +12,8 @@ export default App
 */
 
 import React, { useState } from 'react';
-import { NavigationContainer } from '@react-navigation/native'
-import { createStackNavigator } from '@react-navigation/stack'
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import SplashScreen from './src/screens/SplashScreen';
 import LoginScreen from './src/screens/LoginScreen';
 import HomeScreen from './src/screens/HomeScreen';
