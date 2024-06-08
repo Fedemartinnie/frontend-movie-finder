f6cc6bc508cd1854f10e902be0ef674c6ae862dc
Fedemartinnie <federicom.nieva@gmail.com>
2024-06-08 00:44:16 -0300

    svg Lapiz + profileScreen

---
+++ src/assets/lapiz.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Svg, {Path} from 'react-native-svg'
+
+export const EditProfile = () => (
+    <Svg width="41" height="38" viewBox="0 0 41 38" fill="none">
+        <Path fill-rule="evenodd" clip-rule="evenodd" d="M0.0258865 37.4514H40.0259V33.7713H0.0258865V37.4514ZM19.7099 26.411H12.0259V18.8594L31.7039 0.451424L40.0259 8.0656L19.7099 26.411Z" fill="#D9D9D9"/>
+    </Svg>
+
+
+)
+
--- src/screens/ProfileScreen.tsx
+++ src/screens/ProfileScreen.tsx
@@ -1,20 +1,113 @@
-import React from 'react'
-import { View, Text, StatusBar, SafeAreaView, StyleSheet } from 'react-native'
+import React, { useState } from 'react'
+import { View, Text, StatusBar, SafeAreaView, StyleSheet, Image, TouchableOpacity } from 'react-native'
 import NavBar from '../components/home/navBar'
-
+import { CamAvatar } from '../assets/camAvatar'
+import { TextInput, TouchableWithoutFeedback } from 'react-native-gesture-handler'
+import { EditProfile } from '../assets/lapiz'
 
 
 function ProfileScreen(): React.JSX.Element {
+    const[edit, setEdit] = useState<boolean>(false)
+    const [firstName, setFirstName] = useState('María José')
+    const [lastName, setLastName] = useState('Diaz')
 
+    const handleEdit = () => {
+        setEdit(!edit)
+    }
+
+    //!!!TODO
+    const handleSubmit = () => {
+        // Aquí puedes manejar el envío de los datos, por ejemplo, una llamada a una API
+        console.log('First Name:', firstName)
+        console.log('Last Name:', lastName)
+        // Puedes desactivar el modo de edición después de enviar los datos
+        setEdit(false)
+    }
 
     return (
-        <SafeAreaView style={[{ flex: 1}]}>
-            <StatusBar
-            />                       
-            <View style={styles.sectionError}>
-                <Text style={{color: colors.red}}></Text>
-            </View>          
-            <NavBar/>                       
+        <SafeAreaView style={styles.safeContainer}>
+            <StatusBar/>
+            <View style={{flex: 1}}>
+                <View style={styles.profileContainer}>
+
+                    <View style={styles.imageView}>
+                        <Image
+                            source={{ uri: 'https://www.pngitem.com/pimgs/m/501-5015090_ironman-helmet-png-image-iron-man-face-png.png' }} // Cambia esto por la URL correcta de la imagen
+                            style={styles.image}
+                        />
+                        <View style={styles.cameraIcon}>
+                            <CamAvatar />
+                        </View>
+                    </View>
+
+                    {!edit 
+                        ? (
+                            <View style={[styles.userInfo, {marginLeft: '11%'}]}>
+                                <Text style={styles.userName}>Fede Nie</Text>
+                            
+                                <TouchableOpacity onPress={handleEdit}>
+                                    <View style={styles.editIcon}>
+                                        <EditProfile />
+                                    </View>
+                                </TouchableOpacity>                                           
+                            </View>)
+                        : (
+                            <View style={[styles.userInfo]}>
+                                <Text style={styles.userName}>Fede Nie</Text>
+                                <View style={styles.editIcon}>
+                                        
+                                </View>
+                            </View>)
+                    }
+                </View>
+
+                <View style={styles.options}>
+                    {!edit 
+                        ? (<>
+                            <Text style={styles.optionText}>María José</Text>
+                            <Text style={styles.optionText}>Diaz</Text>
+                            <Text style={styles.optionText}>Cerrar Sesión</Text>
+                            <Text style={styles.optionTextDanger}>Eliminar Cuenta</Text>
+                        </>
+                        )
+                        : (<>
+                            <TextInput 
+                                style={styles.optionText}
+                                value={firstName}
+                                onChangeText={setFirstName}
+                                onSubmitEditing={handleSubmit}
+                                returnKeyType="done"
+                            >
+                                María José
+                            </TextInput>
+                            <TextInput 
+                                style={styles.optionText}
+                                value={lastName}
+                                onChangeText={setLastName}
+                                onSubmitEditing={handleSubmit}
+                                returnKeyType="done"
+                            >
+                                    Diaz
+                            </TextInput>
+                            <Text style={styles.optionText}>Cerrar Sesión</Text>
+                            <Text style={styles.optionTextDanger}>Eliminar Cuenta</Text>
+                        </>
+                        )
+
+                    }
+                    
+                </View>
+                {edit && (
+                    //!* aca poner el button cancelar con space-between
+                    <View style={styles.saveContainer}>
+                        <TouchableOpacity onPress={handleSubmit}>
+                            <Text style={styles.saveButton}>Guardar</Text>
+                        </TouchableOpacity>
+                    </View>
+                )}
+                
+            </View>
+            <NavBar />
         </SafeAreaView>
     )
 }
@@ -27,16 +120,82 @@ const colors = {
     blueDark: '#052539',
 }
 
-const styles = StyleSheet.create({    
-    sectionError: {
+const styles = StyleSheet.create({
+    safeContainer: {
+        flex: 1,
+        backgroundColor: colors.blueDark,
+    },
+    profileContainer: {
         alignItems: 'center',
+        marginVertical: 50,
     },
-    sectionNavbar: {
-
+    imageView: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        position: 'relative',
     },
-    container: {
-        backgroundColor: '#052539',
+    image: {
+        width: 200,
+        height: 200,
+        borderRadius: 100,
     },
+    cameraIcon: {
+        position: 'absolute',
+        bottom: 0,
+        right: 0,
+        backgroundColor: '#3C0C79',
+        borderRadius: 12,
+        padding: 2,
+    },
+    userInfo: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginTop: 20,
+    },
+    userName: {
+        color: '#fff',
+        fontSize: 30,
+        fontWeight: 'bold',
+    },
+    editIcon: {
+        marginLeft: 15,
+    },
+    options: {
+        marginTop: 20,
+    },
+    optionText: {
+        color: '#fff',
+        fontSize: 16,
+        borderBottomColor: '#fff',
+        borderBottomWidth: 1,
+        paddingVertical: 20,
+        marginHorizontal: 25,
+    },
+    optionTextDanger: {
+        color: 'red',
+        fontSize: 16,
+        borderBottomColor: '#fff',
+        borderBottomWidth: 1,
+        paddingVertical: 20,
+        marginHorizontal: 25,
+    },
+    saveContainer:{
+        // alignContent: 'flex-end',
+        alignItems: 'flex-end',
+        margin: 30
+    },
+    saveButton: {
+        color: 'yellow',
+        fontSize: 24,
+        // fontWeight: 'bold'
+        textAlign: 'center',
+        borderColor: 'white',
+        borderWidth: 2,
+        borderRadius: 25,
+        paddingVertical: 5,
+        paddingHorizontal: 15
+    }
+    
 })
 
 
