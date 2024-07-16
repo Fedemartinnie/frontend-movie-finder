65232d0e0f167ac68f3445cc4f92232c7091ab6f
IARDGT <ivanantoniord@hotmail.com>
2024-07-16 20:44:03 -0300

    ip aws apk release

--- src/services/favorites.ts
+++ src/services/favorites.ts
@@ -10,9 +10,9 @@ interface Config {
 }
 
 //const URI = 'http://192.168.0.73:8000' //! ip fede
-const URI = 'http://192.168.1.3:8000' //! ip jere
-// const URI = 'http://18.221.46.103:8000' //* AWS ip
-// const URI = 'http://192.168.1.48:8000' //* ip ivan
+//const URI = 'http://192.168.1.3:8000' //! ip jere
+const URI = 'http://3.140.255.162:8000' //* AWS ip
+//const URI = 'http://192.168.1.48:8000' //* ip ivan
 
 
 //*TODO
